1edb5a877f6f0c39e2e5224205adc1d86ccf15a1
aseem sethi <aseemsethi@yahoo.com>
2019-02-04 11:48:04 +0530

    Made the App multi tenant Establishment, indexed on Establishment

--- app/src/main/java/com/example/ea/start_main.java
+++ app/src/main/java/com/example/ea/start_main.java
@@ -98,7 +98,7 @@ public class start_main extends AppCompatActivity {
             @Override public void onClick(View v) {
                 Log.v(TAG, "Get Database entries");
                 deleteAllTableRows();
-                String cp = "User/Photos/" + current_user;
+                String cp = current_ea + "/Photos/" + current_user;
                 db.collection(cp)
                         .get()
                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -179,6 +179,7 @@ public class start_main extends AppCompatActivity {
             }
         });
 
+        // Save to DB
         Button saveRowButton = (Button) findViewById(R.id.table_layout_save_row_button);
         saveRowButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -200,6 +201,7 @@ public class start_main extends AppCompatActivity {
                                     Log.v(TAG, "Photo: " + v2.getText().toString());
                                     addNewContact();
                                     addNewPhoto(v1.getText().toString(), v2.getText().toString());
+                                    stk.removeViewAt(i);
                                     break;
                                 }
                             }
@@ -234,7 +236,7 @@ public class start_main extends AppCompatActivity {
                                 // If columns is a checkbox and checked then save the row number in list.
                                 CheckBox checkboxView = (CheckBox) columnView;
                                 if (checkboxView.isChecked()) {
-                                    final String cp = "User/Photos/" + current_user;
+                                    final String cp = current_ea + "/Photos/" + current_user;
                                     TextView firstTextView = (TextView) tableRow.getChildAt(0);
                                     String firstText = firstTextView.getText().toString();
                                     Log.v(TAG, "Searching for : " + firstText);
@@ -338,11 +340,10 @@ public class start_main extends AppCompatActivity {
         Map<String, String> newContact = new HashMap<>();
         newContact.put("Name", current_user);
         newContact.put("id", current_uid);
-        db.collection("User").document(current_user).set(newContact)
+        String cp = current_ea;
+        db.collection(cp).document(current_user).set(newContact)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override public void onSuccess(Void aVoid) {
-                        Toast.makeText(start_main.this, "User Registered",
-                                Toast.LENGTH_SHORT).show();
                         Log.v(TAG, "Added Contact to DB");
                     }
                 })
@@ -361,67 +362,26 @@ public class start_main extends AppCompatActivity {
         newPhoto.put("photoNo", photoNo);
         newPhoto.put("photoText", photoText);
         String docId = db.collection("User").document(current_user).getId();
-        String cp = "User/Photos/" + docId;
+        String cp = current_ea + "/Photos/" + docId;
         Log.v(TAG, "Sub Collection at: " + cp);
         db.collection(cp).document().set(newPhoto)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override public void onSuccess(Void aVoid) {
-                        Toast.makeText(start_main.this, "Photo Added",
-                                Toast.LENGTH_SHORT).show();
+                        Toast toast = Toast.makeText(start_main.this, "Photo Added to Gallery",
+                                Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                        toast.show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(start_main.this, "ERROR" + e.toString(),
-                                Toast.LENGTH_SHORT).show();
+                        Toast toast = Toast.makeText(start_main.this, "ERROR" + e.toString(),
+                                Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                        toast.show();
                         Log.v(TAG, e.toString());
                     }
                 });
     }
-
-    public void init_table(){
-        TableLayout stk = (TableLayout) findViewById(R.id.table_layout_table);
-        TableRow tbrow0 = new TableRow(this);
-
-        TextView tv0 = new TextView(this);
-        tv0.setText(" No. "); tv0.setTextColor(Color.WHITE);
-        tbrow0.addView(tv0);
-
-        TextView tv1 = new TextView(this);
-        tv1.setText(" Photo ID "); tv1.setTextColor(Color.WHITE);
-        tbrow0.addView(tv1);
-
-        TextView tv2 = new TextView(this);
-        tv2.setText(" Token No. "); tv2.setTextColor(Color.WHITE);
-        tbrow0.addView(tv2);
-
-        TextView tv3 = new TextView(this);
-        tv3.setText(" Comments "); tv3.setTextColor(Color.WHITE);
-        tbrow0.addView(tv3); stk.addView(tbrow0);
-        tbrow0.setBackgroundResource(R.drawable.row_border);
-
-        for (int i = 0; i < 5; i++) {
-            TableRow tbrow = new TableRow(this);
-
-            TextView t1v = new TextView(this);
-            t1v.setText("" + i); t1v.setTextColor(Color.WHITE); t1v.setGravity(Gravity.CENTER);
-            tbrow.addView(t1v);
-
-            TextView t2v = new TextView(this);
-            t2v.setText("Product " + i); t2v.setTextColor(Color.WHITE); t2v.setGravity(Gravity.CENTER);
-            tbrow.addView(t2v);
-
-            TextView t3v = new TextView(this);
-            t3v.setText("Rs." + i); t3v.setTextColor(Color.WHITE); t3v.setGravity(Gravity.CENTER);
-            tbrow.addView(t3v);
-
-            TextView t4v = new TextView(this);
-            t4v.setText("" + i * 15 / 32 * 10); t4v.setTextColor(Color.WHITE);
-            t4v.setGravity(Gravity.CENTER); tbrow.addView(t4v);
-            stk.addView(tbrow);
-            tbrow.setBackgroundResource(R.drawable.row_border);
-        }
-    }
-
 }
